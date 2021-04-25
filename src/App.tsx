62c027fafad46b53e8152f4f5f4408610f6635bb
Xiang Chen <xiangchenchen96@gmail.com>
2021-04-26 00:12:53 +0200

    :sparkles: Add simple canvas clock with sweetalert alarms

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Swal from "sweetalert2";
+import moment from "moment-timezone";
+
+import Clock from "./components/Clock";
+
+import "./App.css";
+
+const ALARMS_URL =
+  "https://raw.githubusercontent.com/medlabmg/developers-tests/master/frontend/alarm.json";
+
+type Alarm = {
+  id: number;
+  time: string;
+  message: string;
+  active: boolean;
+};
 
 function App() {
+  const [time, setTime] = useState(moment());
+  const [alarms, setAlarms] = useState<Alarm[]>([]);
+  const [timezone, setTimezone] = useState(moment.tz.guess());
+
+  useEffect(() => {
+    async function fetchAlarms() {
+      let response: any = await fetch(ALARMS_URL);
+      response = await response.json();
+      setAlarms(response);
+    }
+    fetchAlarms();
+    setInterval(() => setTime(moment().tz(timezone)), 1000);
+  }, []);
+
+  useEffect(() => {
+    if (time.seconds() === 0) checkAlarms();
+  }, [time]);
+
+  const checkAlarms = () => {
+    const hour = time.hours();
+    const minute = time.minutes();
+    const alerts: string[] = [];
+    alarms
+      .filter((a) => a.active)
+      .forEach((a) => {
+        const [alarmHour, alarmMinute] = a.time.split(":").map(Number);
+        if (alarmHour === hour && alarmMinute === minute)
+          alerts.push(a.message);
+      });
+    Swal.queue(alerts);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Clock time={time} />
     </div>
   );
 }
---
+++ src/components/Clock.tsx
@@ -0,0 +1,117 @@
+import React, { useRef, useEffect } from "react";
+import { Moment } from "moment-timezone";
+
+// This clock is based on the tutorial from w3schools
+// see: https://www.w3schools.com/graphics/canvas_clock.asp
+
+const draw = (ctx: CanvasRenderingContext2D, radius: number, time: Moment) => {
+  drawFace(ctx, radius);
+  drawNumbers(ctx, radius);
+  drawTime(ctx, radius, time);
+};
+
+const drawFace = (ctx: CanvasRenderingContext2D, radius: number) => {
+  let grad;
+
+  ctx.beginPath();
+  ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+  ctx.fillStyle = "white";
+  ctx.fill();
+
+  grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
+  grad.addColorStop(0, "#333");
+  grad.addColorStop(0.5, "white");
+  grad.addColorStop(1, "#333");
+  ctx.strokeStyle = grad;
+  ctx.lineWidth = radius * 0.1;
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+  ctx.fillStyle = "#333";
+  ctx.fill();
+};
+
+const drawNumbers = (ctx: CanvasRenderingContext2D, radius: number) => {
+  var ang;
+  var num;
+  ctx.font = radius * 0.15 + "px arial";
+  ctx.textBaseline = "middle";
+  ctx.textAlign = "center";
+  for (num = 1; num < 13; num++) {
+    ang = (num * Math.PI) / 6;
+    ctx.rotate(ang);
+    ctx.translate(0, -radius * 0.85);
+    ctx.rotate(-ang);
+    ctx.fillText(num.toString(), 0, 0);
+    ctx.rotate(ang);
+    ctx.translate(0, radius * 0.85);
+    ctx.rotate(-ang);
+  }
+};
+
+const drawTime = (
+  ctx: CanvasRenderingContext2D,
+  radius: number,
+  now: Moment
+) => {
+  var hour = now.hours();
+  var minute = now.minutes();
+  var second = now.seconds();
+  //hour
+  hour = hour % 12;
+  hour =
+    (hour * Math.PI) / 6 +
+    (minute * Math.PI) / (6 * 60) +
+    (second * Math.PI) / (360 * 60);
+  drawHand(ctx, hour, radius * 0.5, radius * 0.07);
+  //minute
+  minute = (minute * Math.PI) / 30 + (second * Math.PI) / (30 * 60);
+  drawHand(ctx, minute, radius * 0.8, radius * 0.07);
+  // second
+  second = (second * Math.PI) / 30;
+  drawHand(ctx, second, radius * 0.9, radius * 0.02);
+};
+
+const drawHand = (
+  ctx: CanvasRenderingContext2D,
+  pos: number,
+  length: number,
+  width: number
+) => {
+  ctx.beginPath();
+  ctx.lineWidth = width;
+  ctx.lineCap = "round";
+  ctx.moveTo(0, 0);
+  ctx.rotate(pos);
+  ctx.lineTo(0, -length);
+  ctx.stroke();
+  ctx.rotate(-pos);
+};
+
+const Canvas = (props: any) => {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas: any = canvasRef.current;
+    const context = canvas.getContext("2d");
+    let radius = canvas.height / 2;
+    context.translate(radius, radius);
+    radius = radius * 0.9;
+
+    draw(context, radius, props.time);
+  }, []);
+
+  useEffect(() => {
+    const canvas: any = canvasRef.current;
+    const context = canvas.getContext("2d");
+    let radius = canvas.height / 2;
+    radius = radius * 0.9;
+
+    draw(context, radius, props.time);
+  }, [draw, props.time]);
+
+  return <canvas ref={canvasRef} />;
+};
+
+export default Canvas;
